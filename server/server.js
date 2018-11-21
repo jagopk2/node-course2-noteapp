571909e2671769f15918fa5a006f4f6aa31313b1
Your Name <you@example.com>
2018-11-21 18:29:29 +0500

    75-list-Resources

--- server/server.js
+++ server/server.js
@@ -20,6 +20,18 @@ app.post('/todos',(req,res)=>{
   });
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    },(err) =>{
+        req.status(400).send(err);
+    });
+    
+});
+
 app.listen(3000,()=>{
  console.log('Server Started on port 3000');
 }); 
+
+
+module.exports = {app};
